813d65f3f6d30109ab17b6efc0c8e9d7aba8769c
Andreas Blaesius <skate4life@gmx.de>
2017-03-03 13:30:07 +0000

    maguro: guard the makefile
    Change-Id: Idceb7f9fc78e716f54675698e5d3bf5e74369af9

--- Android.mk
+++ Android.mk
@@ -25,4 +25,8 @@ LOCAL_PATH := $(call my-dir)
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
 
+ifeq ($(TARGET_DEVICE),maguro)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
